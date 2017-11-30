52d140dedaac0c80c1a59fb6ad62e6174f692b51
Wendy <wendy_siu@berkeley.edu>
2017-11-30 01:52:12 -0800

    Initial Commit MPC in Python

---
+++ MPCtest.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.optimize import minimize
+from scipy.spatial import ConvexHull
+
+#initialize variables
+image_list=[]
+path_width = 5
+nR = 8
+nC = 10
+
+# forward pass
+for x in range(int(nR/2)):
+    #define image_row
+    image_row = np.zeros(nC)
+    #forward pass
+    if x+path_width > nC:
+        for index in range(x,nC):
+            image_row[index] = 1
+    else:
+        for index in range(x,x+path_width):
+                image_row[index] = 1
+    image_list.append(image_row)
+
+# stack and flip
+image_all = np.vstack(image_list)
+image_flip = np.flipud(image_all)
+image_all = np.vstack((image_all,image_flip))
+
+# determine row and column indicies
+outline = (np.diff(np.sign(image_all)) != 0)*1
+
+indexes = np.where(outline == 1)
+points1 = np.transpose(np.vstack(indexes))
+
+# determine convex hull from points
+points2 = np.random.rand(30, 2) 
+hull = ConvexHull(points1)
+
+import matplotlib.pyplot as plt
+plt.plot(points1[hull.vertices,0], points1[hull.vertices,1], 'r--', lw=2)
+plt.plot(points1[hull.vertices[0],0], points1[hull.vertices[0],1], 'ro')
+plt.show()
